38c59d55fda91dac59d2d3d7cb5950c9cf9f8037
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-08 15:07:57 -0300

    deps: add @radix-ui/react-dialog to deps

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./button";
+
 export function Header() {
   return (
-    <header className="w-full py-2 flex items-center justify-center bg-zinc-900 text-white">
-      <div className="w-[90%] max-w-[720px] flex items-center justify-between">
-        <h1>CBook</h1>
+    <header className="w-full py-2 flex items-center justify-center bg-zinc-900 text-white shadow-md">
+      <div className="w-[90%] max-w-[720px] flex items-center justify-between py-2">
+        <Link href="/">
+          <Image src="/logo.svg" width={60} height={24} alt="Logo" />
+        </Link>
 
         <div className="flex items-center gap-2">
-          <button>Login</button>
-          <button>Signup</button>
+          <Link href="/login">
+            <Button variant="primary">Login</Button>
+          </Link>
+
+          <Link href="/signup">
+            <Button variant="primary">Signup</Button>
+          </Link>
         </div>
       </div>
     </header>
