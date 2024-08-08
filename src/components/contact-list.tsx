6b9c2ce528e12529ce637101b8437c755e434b4f
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-08 15:09:00 -0300

    feat(components): add contact-list component

--- src/components/contact-list.tsx
+++ src/components/contact-list.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Contact, ContactData } from "./contact";
 
 interface ContactListProps {
@@ -9,17 +10,21 @@ interface ContactListProps {
 export function ContactList({ contacts, onEditRequest, onDeleteRequest }: ContactListProps) {
   return (
     <ul className="w-full flex flex-col items-center gap-1">
-      {contacts.map((c) => (
-        <Contact
-          id={c.id}
-          key={c.id}
-          name={c.name}
-          email={c.email}
-          phoneNumber={c.phone_number}
-          onEditRequest={onEditRequest}
-          onDeleteRequest={onDeleteRequest}
-        />
-      ))}
+      {contacts.length ? (
+        contacts.map((c) => (
+          <Contact
+            id={c.id}
+            key={c.id}
+            name={c.name}
+            email={c.email}
+            phoneNumber={c.phone_number}
+            onEditRequest={onEditRequest}
+            onDeleteRequest={onDeleteRequest}
+          />
+        ))
+      ) : (
+        <Image src="/empty-amico.svg" width={256} height={256} alt="Empty" />
+      )}
     </ul>
   );
 }
