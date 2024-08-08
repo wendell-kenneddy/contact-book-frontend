c9508585e84bc03cd3fe5aafa2ecf2e4aabff565
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-08 15:10:52 -0300

    feat(data): add mock contacts data

---
+++ src/data/contacts.ts
@@ -0,0 +1,64 @@
+import { ContactData } from "../components/contact";
+
+export const contacts: ContactData[] = [
+  {
+    id: "207834af-607d-466b-985f-3003168f241a",
+    name: "Adam Smasher",
+    email: "adam@email.com",
+    phone_number: "1231092380",
+  },
+  {
+    id: "645718a0-a5bf-47bf-9a53-1ee69fa7900f",
+    name: "John Doe",
+    email: "john@email.com",
+    phone_number: "1234567890",
+  },
+  {
+    id: "9c5bee22-bd38-4c9c-8f33-8965aed88fa4",
+    name: "Mary Doe",
+    email: "mary@email.com",
+    phone_number: "0987654321",
+  },
+  {
+    id: "843c00df-5b74-4335-8542-5dce8b90c39a",
+    name: "Kim",
+    email: "kim@email.com",
+    phone_number: "1234509876",
+  },
+  {
+    id: "491cb1bf-7088-4e10-ac40-c668b6c14911",
+    name: "Steve Jobs",
+    email: "steve@email.com",
+    phone_number: "0987612345",
+  },
+  {
+    id: "9d5d3143-1346-4015-a2a4-3774105bdd76",
+    name: "Simon Pilkings",
+    email: "simon@email.com",
+    phone_number: "8981201230",
+  },
+  {
+    id: "d9e7d0f6-0ed6-40d4-8bbc-0c31cbd394d2",
+    name: "Jimmy Neutron",
+    email: "jimmy@email.com",
+    phone_number: "1029381029",
+  },
+  {
+    id: "5edfea02-9df8-44fc-accb-774b6b46ef92",
+    name: "Tin",
+    email: "tin@email.com",
+    phone_number: "1919022910",
+  },
+  {
+    id: "a1c5382b-6e3d-4c04-8ec4-07c571644064",
+    name: "Gallagher",
+    email: "gallagher@email.com",
+    phone_number: "1293102983",
+  },
+  {
+    id: "b3e1cc36-6ac6-49b7-b018-f17e757df82c",
+    name: "Marin",
+    email: "marin@email.com",
+    phone_number: "1230912831",
+  },
+];
