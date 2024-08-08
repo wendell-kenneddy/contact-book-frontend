28e1f3cb836531dc3397c8860cb97ed7c5dee633
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-08 15:08:38 -0300

    feat(components): add contact component

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -23,14 +23,12 @@ export function Contact({
   onDeleteRequest,
 }: ContactProps) {
   return (
-    <li className="w-full grid items-center grid-cols-3 md:grid-cols-4 bg-zinc-900 rounded-md py-2 px-4 shadow-lg">
-      <span className="text-zinc-100/50 text-xs md:text-sm text-center">{name}</span>
+    <li className="w-full text-zinc-100/50 hover:text-zinc-100/100 grid items-center grid-cols-3 md:grid-cols-4 bg-zinc-900 rounded-md py-2 px-4 shadow-lg">
+      <span className="text-xs md:text-sm text-center">{name}</span>
 
-      <span className="text-zinc-100/50 text-xs md:text-sm sr-only md:not-sr-only text-center">
-        {email}
-      </span>
+      <span className="text-xs md:text-sm sr-only md:not-sr-only text-center">{email}</span>
 
-      <span className="text-zinc-100/50 text-xs md:text-sm text-center">{phoneNumber}</span>
+      <span className="text-xs md:text-sm text-center">{phoneNumber}</span>
 
       <div className="flex items-center justify-center gap-1">
         <Button name="Edit contact" variant="secondary" onClick={() => onEditRequest(id)}>
