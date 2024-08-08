fe4e92d2f59167f71c10cdb0bca3a9595dc6bf5a
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-08 15:11:49 -0300

    feat(pages): add homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import { Button } from "@/components/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1 className="text-white">Hello, world!</h1>;
+  return (
+    <main className="flex flex-col items-center gap-4">
+      <h1 className="text-white text-xl font-medium">Easily manage your contacts</h1>
+
+      <Image src="/chat-illustration.png" width={256} height={256} alt="Chat illustration" />
+
+      <Link href="/dashboard">
+        <Button variant="primary">Get Started</Button>
+      </Link>
+    </main>
+  );
 }
