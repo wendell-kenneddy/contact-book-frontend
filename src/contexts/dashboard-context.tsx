b5bf9a882ff0ebb5a1833bb0e719529ba1d6b154
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-30 16:14:29 -0300

    feat(dashboard-context): use contacts api route

--- src/contexts/dashboard-context.tsx
+++ src/contexts/dashboard-context.tsx
@@ -1,15 +1,15 @@
+"use client";
+
 import { ContactData } from "@/components/contact";
-import { clientFetch } from "@/lib/client-fetch";
-import { env } from "@/lib/env";
-import { deleteCookie } from "cookies-next";
-import { redirect } from "next/navigation";
 import { createContext, ReactNode, useEffect, useState } from "react";
 
 export interface DashboardContextData {
   contacts: ContactData[];
   isFetchingContacts: boolean;
   pageIndex: number;
+  tag: number;
   updatePageIndex: (newIndex: number) => void;
+  incrementTag: () => void;
 }
 
 interface DashboardContextProviderProps {
@@ -22,40 +22,44 @@ export function DashboardContextProvider({ children }: DashboardContextProviderP
   const [contacts, setContacts] = useState<ContactData[]>([]);
   const [isFetchingContacts, setIsFetchingContacts] = useState(false);
   const [pageIndex, setPageIndex] = useState(1);
+  const [tag, setTag] = useState(1);
+
+  function incrementTag() {
+    setTag((prev) => prev + 1);
+  }
 
   useEffect(() => {
     setIsFetchingContacts(true);
 
-    clientFetch(`${env.API_BASE_URL}/contacts`, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      mode: "cors",
-      cache: "no-store",
-      body: JSON.stringify({ pageIndex }),
-    })
+    fetch(`/api/contacts?pageIndex=${pageIndex}`, { cache: "no-store" })
       .then((res) => {
-        if (res.status == 401) {
-          deleteCookie("access-token", { path: "/" });
-          deleteCookie("refresh-token", { path: "/" });
-          redirect("/login");
-        }
-
         return res.json();
       })
-      .then((json) => setContacts(json))
+      .then((data) => {
+        const { contacts }: { contacts: ContactData[] } = data;
+        setContacts(contacts ?? []);
+      })
       .catch((err) => console.log(err));
 
     setIsFetchingContacts(false);
-  }, [pageIndex]);
+  }, [tag, pageIndex]);
 
   function updatePageIndex(newIndex: number) {
     setPageIndex(newIndex);
+    incrementTag();
   }
 
   return (
-    <DashboardContext.Provider value={{ contacts, isFetchingContacts, pageIndex, updatePageIndex }}>
+    <DashboardContext.Provider
+      value={{
+        contacts,
+        isFetchingContacts,
+        pageIndex,
+        tag,
+        updatePageIndex,
+        incrementTag,
+      }}
+    >
       {children}
     </DashboardContext.Provider>
   );
