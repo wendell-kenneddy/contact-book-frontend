df1f3b559aac735db07af11510cdf4e359743f19
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-09-05 09:52:29 -0300

    refactor(dashboard): clean up code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,40 +1,27 @@
 "use client";
 
+import { useDashboard } from "@/hooks/use-dashboard";
+import { ArrowLeft, ArrowRight, Plus } from "@phosphor-icons/react";
+import { useState } from "react";
 import { Button } from "@/components/button";
 import { ContactData } from "@/components/contact";
 import { ContactList } from "@/components/contact-list";
-import { CreateContactModal } from "@/components/create-contact-modal";
 import { DeleteContactDialog } from "@/components/delete-contact-dialog";
 import { EditContactModal } from "@/components/edit-contact-dialog";
-import { contacts } from "@/data/contacts";
-import { ArrowLeft, ArrowRight, Plus, X } from "@phosphor-icons/react";
-import { useState } from "react";
+import { CreateContactModal } from "@/components/create-contact-modal";
 
 export default function Dashboard() {
-  const [seedContacts, setSeedContacts] = useState<ContactData[]>(contacts);
+  const { contacts, pageIndex, updatePageIndex, isFetchingContacts } = useDashboard();
   const [contactToDeleteID, setContactToDeleteID] = useState<string | null>(null);
-  const [contactToEditID, setContactToEditID] = useState<string>("");
+  const [contactToEditID, setContactToEditID] = useState<string | null>(null);
   const [isCreateContactModalOpen, setIsCreateContactModalOpen] = useState(false);
 
-  function closeDeleteContactDialog() {
-    setContactToDeleteID(null);
+  function paginateForward() {
+    updatePageIndex(pageIndex + 1);
   }
 
-  function handleDelete() {
-    setSeedContacts((prev) => prev.filter((c) => c.id != contactToDeleteID));
-    closeDeleteContactDialog();
-  }
-
-  function openCreateContactModal() {
-    setIsCreateContactModalOpen(true);
-  }
-
-  function closeCreateContactModal() {
-    setIsCreateContactModalOpen(false);
-  }
-
-  function closeEditContactModal() {
-    setContactToEditID("");
+  function paginateBackwards() {
+    updatePageIndex(pageIndex - 1);
   }
 
   return (
@@ -42,42 +29,65 @@ export default function Dashboard() {
       <div className="flex items-center gap-2">
         <h1 className="text-xl font-medium">Your contacts</h1>
 
-        <Button name="Add contact" variant="secondary" onClick={openCreateContactModal}>
+        <Button
+          name="Add contact"
+          variant="secondary"
+          onClick={() => setIsCreateContactModalOpen(true)}
+        >
           <Plus className="size-4 fill-teal-400" />
         </Button>
       </div>
 
-      <ContactList
-        contacts={seedContacts}
-        onEditRequest={(id: string) => setContactToEditID(id)}
-        onDeleteRequest={(id: string) => setContactToDeleteID(id)}
-      />
+      {isFetchingContacts ? (
+        <span className="font-medium text-gray-100">Just a moment...</span>
+      ) : (
+        <ContactList
+          contacts={contacts}
+          onEditRequest={(id: string) => setContactToEditID(id)}
+          onDeleteRequest={(id: string) => setContactToDeleteID(id)}
+        />
+      )}
 
       <div className="flex w-full items-center justify-between">
-        <Button variant="secondary">
+        <Button
+          variant="secondary"
+          disabled={isFetchingContacts || pageIndex == 1}
+          onClick={paginateBackwards}
+        >
           <ArrowLeft className="size-6 fill-teal-400" fill="bold" />
         </Button>
 
-        <Button variant="secondary">
+        <Button
+          variant="secondary"
+          disabled={isFetchingContacts || contacts.length < 10}
+          onClick={paginateForward}
+        >
           <ArrowRight className="size-6 fill-teal-400" fill="bold" />
         </Button>
       </div>
 
-      <DeleteContactDialog
-        open={!!contactToDeleteID}
-        onClose={closeDeleteContactDialog}
-        onDelete={handleDelete}
-      />
+      {!!contactToDeleteID && (
+        <DeleteContactDialog
+          open={!!contactToDeleteID}
+          contactID={contactToDeleteID}
+          onClose={() => setContactToDeleteID(null)}
+        />
+      )}
 
       {!!contactToEditID && (
         <EditContactModal
           open={true}
-          contact={seedContacts.find((c) => c.id == contactToEditID) as ContactData}
-          closeModalFunction={closeEditContactModal}
+          contact={contacts.find((c) => c.id == contactToEditID) as ContactData}
+          onClose={() => setContactToEditID(null)}
         />
       )}
 
-      <CreateContactModal open={isCreateContactModalOpen} onClose={closeCreateContactModal} />
+      {isCreateContactModalOpen && (
+        <CreateContactModal
+          open={isCreateContactModalOpen}
+          onClose={() => setIsCreateContactModalOpen(false)}
+        />
+      )}
     </main>
   );
 }
